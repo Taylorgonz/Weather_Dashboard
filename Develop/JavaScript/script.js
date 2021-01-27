3676060f3e7f7d30904bce4c7ca811af9610819f
Taylor Gonzales <hello@taylorgonz.com>
2021-01-27 16:03:11 -0500

    folders and files created

---
+++ Develop/JavaScript/script.js
@@ -0,0 +1,3 @@
+const currentWeather = `api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid={API key}`
+const fiveDay = `api.openweathermap.org/data/2.5/forecast?q={city name},{state code}&appid={API key}`
+const myKey  ="9a10739234e04a70af0f9f8db26046ea"
