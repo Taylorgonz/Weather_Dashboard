15e12662fb1d9f8b47ee9b4b9e22215021814a8a
Taylor Gonzales <hello@taylorgonz.com>
2021-01-31 23:50:09 -0500

    file clean up and read me updated

--- Develop/JavaScript/script.js
+++ Develop/JavaScript/script.js
@@ -2,162 +2,177 @@ const currentWeather = `api.openweathermap.org/data/2.5/weather?q={city name},{s
 const fiveDay = `api.openweathermap.org/data/2.5/forecast?q={city name},{state code}&appid={API key}`;
 const myKey = "1f8d47fa1e0ef87f64e23560949b1f1c";
 let cityInput = $("#city-value");
-let stateInput = $("#state-value");
-let buttonArray =  JSON.parse(localStorage['savedCities'] || "[]");
+let buttonArray = JSON.parse(localStorage["savedCities"] || "[]");
 let buttonInput = JSON.parse(localStorage.getItem("savedCities"));
 let id = 0;
+const fiveDayArr = [3, 11, 19, 27, 35];
 
 // listening for search button slick
 
+$(document).ready(function () {
+init();
 
-// $(document).ready(function () {
-  init();
+renderButtons();
 
-  renderButtons();
-
-  
+console.log(fiveDayArr)
 // initial function to display last searchs
-  function init() {
-    if ( buttonArray !== null) {
+function init() {
+
+$('#input-form').append('<button id="search" type="button" class="search button is-primary"> Search </button> <div class="column"> <input id="city-value" class="city-value input is-primary" type="text" placeholder="City"/> </div>')
+
+  if (buttonArray !== null) {
     let i = parseInt(buttonArray.length);
-    i --; 
-    cityInput= buttonInput[i].city;
-    stateInput = buttonInput[i].state;
-  
+    i--;
+    cityInput = buttonInput[i].city;
+
     weatherCall();
+    
   }
+}
+
+// function search() {
+$(".search").on("click", function (e) {
+  cityInput = cityInput.val();
+
+  console.log(cityInput);
+  if (cityInput !== "") {
+    // retrieving value from city and state inputs
+
+    // cityInput = cityInput.val();
+    // stateInput = stateInput.val();
+
+    storedCityState = {
+      city: cityInput,
+      state: stateInput,
+    };
+
+    buttonArray.push(storedCityState);
+    localStorage.setItem("savedCities", JSON.stringify(buttonArray));
+
+    init();
+    renderButtons();
   }
 
+  // creates input buttons
 
-  // function search() {
-    $(".search").on( "click", function(e) {
-      
-      cityInput = cityInput.val();
-      stateInput = stateInput.val();
+});
+// }
 
-      console.log(cityInput);
-      if (cityInput !== "" && stateInput !== "") {
-        // retrieving value from city and state inputs
-        
-        // cityInput = cityInput.val();
-        // stateInput = stateInput.val();
+// -------------------------------------------------------------
 
-        storedCityState = {
-          city: cityInput,
-          state: stateInput,
-        };
+// function for ajax call for current weather
+function weatherCall() {
+  // current day weather ajax call
+  $.get("https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&units=imperial&appid=" + myKey + "" ).then(function(response) {
+    console.log(response);
 
-        buttonArray.push(storedCityState);
-        localStorage.setItem("savedCities", JSON.stringify(buttonArray));
-
-        weatherCall();
-        renderButtons();
-        };
-        
-
-        // creates input buttons
-        
-        $('form')[0].reset();
-      
-    });
-  // }
- 
-
-  // function for ajax call for current weather
-  function weatherCall() {
-
-
-    
-    $.get("https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&" + stateInput +"&units=imperial&appid=" +myKey +"").then(function (response) {
-      console.log(response);
-
-
-      let weatherDisplay = $('<div class=" current-weather card-content" style="background-color: hsl(171, 100%, 41%); border-radius: 10px;">')
-      // let tempF = Math.floor((response.main.temp - 273.15) *1.80 + 32);
-      const lat = response;
-      const city = $('<h2>').text(response.name);
-      const icon = $('<img>').attr("src", "http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png");
-      const temp = $('<p>').text("Temperature: " + Math.floor(response.main.temp));
-      const humidity= $('<p>').text("Humdity: " + response.main.humidity);
-      const wind = $('<p>').text("Wind Speed : " + response.wind.speed);
-      // const uvIndex= $.get("http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon=&appid="+ myKey + "")
-      // console.log(temp);
-      weatherDisplay.append(city, icon, temp, humidity, wind)
-      $('.todays-weather').prepend(weatherDisplay);
-      // $(document).ajaxSuccess(search());
-    });
-
-    // ajax call for 5 day forecast
-    $.get("https://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + "&" + stateInput +"&units=imperial&appid=" +myKey +"").then(function (response) {
-      // console.log(response);
-
-
-
-
-      for ( let i = 0; i < 40; i + 8 ) {
-        // console.log(response.list[i]);
-
-        let forecastDisplay = $('<div class="card ">').attr('id', [i]);
-        const date = $('<h2>').text(response.list[i].dt_txt);
-        const icon = $('<img>').attr("src", "http://openweathermap.org/img/wn/" + response.list[i].weather[0].icon + "@2x.png")
-        const temp = $('<p>').text("Temperature: " + Math.floor(response.list[i].main.temp));
-        const humidity= $('<p>').text("Humdity: " + response.list[i].main.humidity);
-        // console.log(date)
-        // console.log(icon)
-        forecastDisplay.append(date, icon, temp, humidity);
-        $('.five-day-forecast').append(forecastDisplay);
-
-      }
-    });
-  
-  };
-
-  // function to have savedbuttons call ajax request
-  $('.saved-locations').on("click",  function () {
-    let buttonInput = JSON.parse(localStorage.getItem("savedCities"))
-    // pulling id from saved buttons
-    let i = $(this).attr("id");
-    cityInput= buttonInput[i].city;
-    stateInput = buttonInput[i].state;
-    // stateInput = $(this).buttonInput[i].state;
-    
-    // console.log(e)
-    
-    weatherCall();
-    $('.todays-weather').empty();
-    $('.five-day-forecast').empty();
+    let weatherDisplay = $(
+      '<div class=" current-weather card-content" style="background-color: hsl(171, 100%, 41%); border-radius: 10px;">'
+    );
+    // let tempF = Math.floor((response.main.temp - 273.15) *1.80 + 32);
+    const lat = response;
+    const city = $("<h2>").text(response.name);
+    const icon = $("<img>").attr(
+      "src",
+      "http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png"
+    );
+    const temp = $("<p>").text(
+      "Temperature: " + Math.floor(response.main.temp)
+    );
+    const humidity = $("<p>").text("Humdity: " + response.main.humidity);
+    const wind = $("<p>").text("Wind Speed : " + response.wind.speed);
+    // const uvIndex= $.get("http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon=&appid="+ myKey + "")
+    // console.log(temp);
+    weatherDisplay.append(city, icon, temp, humidity, wind);
+    $(".todays-weather").prepend(weatherDisplay);
+    // $(document).ajaxSuccess(search());
   });
+  // -----------------
 
-  // ---------------------------------------------------------------
-
-  // render button function
-  function renderButtons() {
-    $(".saved-buttons").empty();
-    $(".clear-button").empty();
-    console.log(buttonInput);
-    if (buttonInput == null || buttonInput == '') return;
+  // ajax call for 5 day forecast
+  $.get(
+    "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + "&" + stateInput + "&units=imperial&appid=" + myKey +"").then(function (response) {
+    // console.log(response);
     
-    else {
-      let clearButton = $('<button>');
-        clearButton.addClass("clear-button button is-danger").text("Clear Cities")
-        $('.clear-button').prepend(clearButton);
-    }
+    for (let i = fiveDayArr[0]; i < fiveDayArr.length ; i ++) {
+      // console.log(response.list[i]);
 
-    for (let i = 0; i < buttonInput.length; i++) {
-      let button = $("<button>");
-      button.attr("id",[i]).addClass("saved-locations button is-primary").text("" + buttonInput[i].city + ", " + buttonInput[i].state + "");
-      // console.log(button);
-      $(".saved-buttons").append(button);
+      // fiveDayArr.forEach((response) => {
 
-    }
+      // if (response.list === fiveDayArr ) {
+      let forecastDisplay = $('<div class="card ">').attr("id",[i] );
+      const date = $("<h2>").text(response.list[i].dt_txt);
+      const icon = $("<img>").attr(
+        "src",
+        "http://openweathermap.org/img/wn/" +
+          response.list[i].weather[0].icon +
+          "@2x.png"
+      );
+      const temp = $("<p>").text(
+        "Temperature: " + Math.floor(response.list[i].main.temp)
+      );
+      const humidity = $("<p>").text(
+        "Humdity: " + response.list[i].main.humidity
+      );
+      // console.log(date)
+      // console.log(icon)
+      forecastDisplay.append(date, icon, temp, humidity);
+      $(".five-day-forecast").append(forecastDisplay);
+      }
+});
+
+// -------------------------------------------------------------
+
+// function to have savedbuttons call ajax request
+$(".saved-locations").on("click", function () {
+  let buttonInput = JSON.parse(localStorage.getItem("savedCities"));
+  // pulling id from saved buttons
+  let i = $(this).attr("id");
+  cityInput = buttonInput[i].city;
+  stateInput = buttonInput[i].state;
+  // stateInput = $(this).buttonInput[i].state;
+
+  // console.log(e)
+
+  weatherCall();
+  $(".todays-weather").empty();
+  $(".five-day-forecast").empty();
+});
+
+// ---------------------------------------------------------------
+
+// render button function
+function renderButtons() {
+
+  $(".saved-buttons").empty();
+  $(".clear-button").empty();
+  console.log(buttonInput);
+
+  if (buttonInput == null || buttonInput == "") return;
+  else {
+    let clearButton = $("<button>");
+    clearButton.addClass("clear-button button is-danger").text("Clear Cities");
+    $(".clear-button").prepend(clearButton);
   }
 
-  $('.clear-button').on("click", function(e) {
-    e.preventDefault();
-    $(".saved-buttons").empty();
-    $(".clear-button").empty();
-    $(".todays-weather").empty();
-    window.localStorage.clear();
+  for (let i = 0; i < buttonInput.length; i++) {
+    let button = $("<button>");
+    button
+      .attr("id", [i])
+      .addClass("saved-locations button is-primary")
+      .text("" + buttonInput[i].city + ", " + buttonInput[i].state + "");
+    // console.log(button);
+    $(".saved-buttons").append(button);
+  }
+};
 
-  })
-// });
+// -------------------------------------------------------------
+
+// clear button function
+$(".clear-button").on("click", function (e) {
+  e.preventDefault();
+  $(".saved-buttons").empty();
+  $(".clear-button").empty();
+  $(".todays-weather").empty();
+  window.localStorage.clear();
+});
--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# Weather_Dashboard
+# Day-Planner
+
+## Let's Check the Weather!
+
+Welcome to my weather Dashboard! you can enter your desired city name and find what your current weather conditions look like, along with the next 5 days forecast as well! 
+
+Below I have a screenshots of the application.
+![alt text](Assets/)
+
+## Instructions
+
+- Type in the city you're looking for
+- click the search button and the info will appear on the page along with a button of the city name you just input
+- the buttons are saved to local storage so don't worry about losing your results if you refresh!
+
+
+
+## Usage
+This is a simple responsive site and has
+ - css
+ - html
+ - bulma
+ - javascript
+ - day.js
+ - openweathermap api
+
+## Credits
+Credit to [Mike Fearnley](https://michaelfearnley.com/) and [Scott Byers](https://github.com/switch120)
+
+and the UNH Fullstack Coding Bootcamp
+
+## Links 
+Here is the link to the deployed site: https://taylorgonz.github.io/Weather_Dashboard/
+## Contact
+
+if you want to contact me for any more questions here are my links!
+
+hello@taylorgonz.com
+<br>
+[Website](http://www.taylorgonz.com)
+
+## Licensing
+ [General Public License](https://opensource.org/licenses/GPL-2.0)
